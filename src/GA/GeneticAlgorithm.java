2c043cf6776c6b7655f2a465d916b11bd88d7584
Frede <frede.berdal@gmail.com>
2023-04-18 12:21:02 +0200

    GA ting

--- src/GA/GeneticAlgorithm.java
+++ src/GA/GeneticAlgorithm.java
@@ -1,9 +1,113 @@
 package GA;
 
+import ImageSegment.ImageSegmentationIO;
+import Utils.Pixel;
+import Utils.Settings;
+import Utils.Tuple;
+import Utils.Utils;
+
+import java.util.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
 public class GeneticAlgorithm {
 
-    public void makePopulation(){
+    private Pixel[][] pixels;
+    private List<Individual> pop;
+    private List<List<Individual>> popRanked;
+    private ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool((Settings.threadSize));
+
+    public GeneticAlgorithm (ImageSegmentationIO imageIO) {
+        this.pixels = imageIO.getPixels();
+    }
+
+    public List<Individual> getPop() {
+        return this.pop;
+    }
+
+    public int getParetoFrontSize() { // Vet ikke hva denne skal gjøre må bytte navn litt
+        return this.popRanked.size();
+    }
+
+    public void runGA() {
+        int genCount = 0;
+        makePop();
+        while (genCount < Settings.genSpan) {
+            System.out.println(genCount); // For oversikt
+            List<Individual> newPop = Collections.synchronizedList(new ArrayList<>());
+            List<Individual> parents = parentSelection(this.pop);
+            for (int i = 0; i < Settings.popSize / 2; i++)  {
+                executor.execute(() -> {  // Denne burde endres
+                    Individual p1 = parents.get(Utils.randomInt(parents.size()));
+                    Individual p2 = parents.get(Utils.randomInt(parents.size()));
+                    Tuple<Individual, Individual> offspring = crossover(p1, p2);
+                    newPop.add(offspring.l);
+                    newPop.add(offspring.r);
+                });
+            }
+            while (newPop.size() != Settings.popSize) {
+                // Sync up ?? Fjern?
+            }
+            for (Individual ind : newPop) {
+                if ( )
+            }
+        }
+    }
+
+
+    public void runNSGA() {
+        int genCount = 0;
+        makePop();
+        rankPop(this.pop);
+        while (genCount < Settings.genSpan) {
+            System.out.println(genCount);
+            List<Individual> newPop = Collections.synchronizedList(new ArrayList<>());
+            List<Individual> parents = parentSelection(this.pop);
+            for (int i = 0; i < Settings.popSize / 2; i++) {
+                executor.execute(() ->{ // Denne burde endres
+                    Individual p1 = parents.get(Utils.randomInt(parents.size()));
+                    Individual p2 = parents.get(Utils.randomInt(parents.size()));
+                    Tuple<Individual, Individual> offspring = crossover(p1, p2);
+                    newPop.add(offspring.l);
+                    newPop.add(offspring.r);
+                });
+            }
+            while (newPop.size() != Settings.popSize){
+                // Sync up??
+            }
+            for (Individual ind : newPop) {
+                if (Utils.randomDouble() < Settings.mutationProb) {
+                    ind.segmentMergeMutation();
+                }
+            }
+            this.pop.addAll(newPop);
+            this.popRanked = rankPop(this.pop);
+            newPopFromRank();
+            genCount++;
+        }
+    }
+
+
+
+    public void makePop(){
 
     }
 
+    public List<List<Individual>> rankPop (List<Individual> pop) {
+        List<List<Individual>> rankedPoP = new ArrayList<>();
+        int rank = 1;
+        while (pop.size() > 0) {
+            List<Individual> domSet = findDomenatingSet(pop);
+            for (Individual i : domSet) {
+                i.setRank(rank);
+            }
+            rankedPoP.add(domSet);
+            pop.removeAll(domSet);
+            rank++;
+        }
+        for (List<Individual> i : rankedPoP) {
+            pop.addAll(i);
+        }
+        return rankedPoP;
+    }
 }
--- src/Utils/Settings.java
+++ src/Utils/Settings.java
@@ -3,4 +3,9 @@ package Utils;
 public class Settings {
     public static final int allowedSegmentSize = 200;
     public static final boolean bestSegment = true;
+    public static final int threadSize = 5;
+    public static final int genSpan = 100;
+    public static final int popSize = 20;
+    public static final double mutationProb = 0.3;
+
 }
